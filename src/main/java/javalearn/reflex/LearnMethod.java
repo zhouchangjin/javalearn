99c6445717cf9141426c86c7aa03443050ba7c8e
Administrator <Administrator@www.myass.com>
2018-04-23 16:16:11 +0800

    utf8

---
+++ src/main/java/javalearn/reflex/LearnMethod.java
@@ -0,0 +1,14 @@
+package javalearn.reflex;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class LearnMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+}
