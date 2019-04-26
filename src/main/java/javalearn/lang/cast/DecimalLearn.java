d7c2a2fa309e5e19d54a8c706b7f67d4f7c085a2
zhouchangjin <iamzhouchangjin@163.com>
2019-04-26 17:41:30 +0800

    小数取整

---
+++ src/main/java/javalearn/lang/cast/DecimalLearn.java
@@ -0,0 +1,12 @@
+package javalearn.lang.cast;
+
+public class DecimalLearn {
+
+	public static void main(String[] args) {
+		
+		float a=1.0f;
+		System.out.println(Math.round(a*100/3)*1.0f/100);
+
+	}
+
+}
